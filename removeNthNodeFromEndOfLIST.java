14263f342e0e51cdf0cd4528a72231abd701727d
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2021-02-15 22:20:36 +0530

    https://leetcode.com/problems/remove-nth-node-from-end-of-list/

---
+++ removeNthNodeFromEndOfLIST.java
@@ -0,0 +1,101 @@
+package linkedlist;
+
+public class removeNthNodeFromEndOfLIST {
+
+    public static class ListNode{
+
+        ListNode next;
+        int val;
+        ListNode(int val){
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode head = null;
+
+    static void insert(int data){
+        ListNode node = new ListNode(data);
+        if(head == null){
+            head = node;
+        }
+        else{
+            ListNode curr = head;
+            while(curr.next!=null){
+                curr = curr.next;
+            }
+            curr.next = node;
+        }
+    }
+
+    static void display(){
+        ListNode curr = head;
+        while(curr!=null){
+
+
+            System.out.print(curr.val + "->");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    public static int findLength(ListNode curr){
+        int length = 0;
+        while(curr!=null){
+            length++;
+            curr = curr.next;
+        }
+
+        return length;
+    }
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+        int length = findLength(head);
+
+        if(head == null || length == 1){
+            return null;
+        }
+
+        if(n == length){
+            head = head.next;
+        }
+
+        else{
+
+            ListNode curr = head;
+            for(int i = 0; i< length - n - 1; i++){
+                curr = curr.next;
+            }
+
+            ListNode next = null;
+            if(curr.next.next!=null){
+                next = curr.next.next;
+                curr.next = next;
+            }
+            else{
+                curr.next = null;
+            }
+        }
+
+        return head;
+
+    }
+
+    public static void main(String[] args) {
+        insert(1);
+        insert(2);
+        insert(3);
+        insert(4);
+        insert(5);
+
+        System.out.println("Before removing:");
+        display();
+
+        System.out.println("After removing:");
+        removeNthFromEnd(head,2);
+        display();
+
+    }
+
+
+}
