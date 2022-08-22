25501343a096da3eff9dc39467cc9448f007b224
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-08-21 23:26:43 -0700

    #20

---
+++ practice_v1/valid_parantheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack = new Stack();
+        
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '[' || c == '{'){
+                stack.add(c);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else if(c == ')'){
+                    if(stack.peek() != '('){
+                        return false;
+                    }
+                }
+                else if(c == ']'){
+                    if(stack.peek() != '['){
+                        return false;
+                    }
+                }
+                else{
+                    if(stack.peek() != '{'){
+                        return false;
+                    }
+                }
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
