b21bea17ca7a4af9dd80145b8dec811e4605bcd3
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-09-10 13:22:50 -0400

    #33

---
+++ practice_v1/search_in_a_rotated_sorted_array.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        if(nums.length == 0){
+            return -1;
+        }
+        
+        int low = 0, high = nums.length - 1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+           
+            if(nums[mid] == target){
+                return mid;
+            }
+            
+           
+            // Left portion of the sorted array
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            
+            // Right portion of the sorted array
+            else{
+               
+                if(target >= nums[mid] && target <= nums[high]){
+                    low = mid +1;
+                }
+                else{
+                    high = mid - 1;
+                }
+                
+                
+            }   
+            
+        }
+        
+        return -1;
+        
+    }
+}
